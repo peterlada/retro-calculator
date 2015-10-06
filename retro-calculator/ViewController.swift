500d139f75ca9ae57465666d16699210b615ddcd
Peter Lada <peter@peterlada.com>
2015-10-05 21:39:23 -0400

    play sound on buttons and keep a running number

--- retro-calculator/ViewController.swift
+++ retro-calculator/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Add = "+"
+        case Subtract = "-"
+        case Empty = ""
+    }
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var buttonSound: AVAudioPlayer!
+    
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var currentOperation = Operation.Empty
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        
+        do {
+            buttonSound = try AVAudioPlayer(contentsOfURL: soundUrl)
+            buttonSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func numberPressed(btn: UIButton!) {
+        buttonSound.play()
+        runningNumber += String(btn.tag)
+        outputLbl.text = runningNumber
     }
-
 
 }
 
